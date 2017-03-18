422c7853f9a9a6b093d37e8ab93006c63ec561c9
shcherbakov <shcherbakov@tosh_note>
2017-03-18 03:59:37 +0300

    Добавлен конфиг Для часового пояса Изменен тип данных  дата и время int добавлены преоброзование даты и времини в текст

--- trener/src/main/java/th/test/trener/util/UDate.java
+++ trener/src/main/java/th/test/trener/util/UDate.java
@@ -16,28 +16,76 @@ import java.util.GregorianCalendar;
 public class UDate {
 
     private UDate() {
+
         GregorianCalendar calendar;
         calendar = new GregorianCalendar(1975,
                 Calendar.DECEMBER, 31);
     }
-    private static long getNowSec(int utc){
-    Date date = new Date();
-    return date.getTime()/ 1000 +utc*3600;
-    }
-    public static long nowDate(int utc) {
-               
-        return getNowSec(utc)/ ( 60 * 60 * 24);
-       
+
+    private static int getNowSec() {
+
+        Date date = new Date();
+        return (int) (date.getTime() / 1000 + Cfg.getUtc() * 3600);
     }
 
-    public static long nowTime(int utc) {
- 
-        return getNowSec(utc) % ( 60 * 60 * 24);
+    public static int nowDate() {
+
+        return getNowSec() / (60 * 60 * 24);
+
     }
 
-    public static String intDataToString(long i,int utc) {
-        Date date = new Date((i*24-utc)*3600000);
-        
+    public static int nowTime() {
+
+        return getNowSec() % (60 * 60 * 24);
+    }
+
+    public static String intDataToString(int i) {
+        Date date = new Date(((long)i * 24 - Cfg.getUtc()) * 3600000);
+
         return date.toString();
+    }
+
+    public static String dataString(int i) {
+        Date date = new Date(((long)i * 24 - Cfg.getUtc()) * 3600000);
+        Calendar myCal = new GregorianCalendar();
+        myCal.setTime(date);
+        String str="";
+        
+        str=myCal.get(Calendar.DAY_OF_MONTH)+"-";
+        if(myCal.get(Calendar.MONTH)<9)   {
+        str+="0";
+        }  
+        str+=(myCal.get(Calendar.MONTH)+1)+"-"+myCal.get(Calendar.YEAR);
+        return str;
+    }
+  public static String timeString(int i) {
+        Date date = new Date((i  - Cfg.getUtc()*3600) * 1000);
+        Calendar myCal = new GregorianCalendar();
+        myCal.setTime(date);
+        String str="";
+        if(myCal.get(Calendar.HOUR_OF_DAY)<10)   {
+        str+="0";
+        } 
+        str=myCal.get(Calendar.HOUR_OF_DAY)+":";
+        if(myCal.get(Calendar.MINUTE)<10)   {
+        str+="0";
+        }  
+        str+=myCal.get(Calendar.MINUTE)+":";
+        if(myCal.get(Calendar.SECOND)<10)   {
+        str+="0";
+        }  str+=myCal.get(Calendar.SECOND);
+        return str;
+    }
+    public static class Cfg {
+
+        private static int utc = 3;
+
+        public static int getUtc() {
+            return utc;
+        }
+
+        public void setUtc(int utc) {
+            this.utc = utc;
+        }
     }
 }
